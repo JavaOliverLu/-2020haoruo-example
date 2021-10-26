2cac4b5d98c579e64261171beb69f80ae837b0a5
JavaOliverLu <70323169+JavaOliverLu@users.noreply.github.com>
2021-10-26 22:34:34 +0800

    Update if 範例.java

--- if 範例.java
+++ if 範例.java
@@ -1,17 +1,14 @@
-public class ch4_05 //主類別
-{
-public static void main(String args[])
-{
-int age=30;
-if(age>18){
-System.out.println("抓去關");
-}
-else if(age>12){
-System.out.println("抓去訓械");
-}
-else{
-System.out.println("無罪釋放");
-
-}
-}
+public class ch4_05 {
+  public static void main(String args[]){
+    int age=30;
+    if(age>18){
+      System.out.println("抓去關");
+      }
+    else if(age>12){
+      System.out.println("抓去訓械");
+      }
+    else{
+      System.out.println("無罪釋放");
+      }
+    }
 }
